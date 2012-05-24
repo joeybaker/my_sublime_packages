4e54a767b3733982224447cca2829a1b7207532a
Joey Baker <joey@byjoeybaker.com>
2012-05-24 12:58:47 -0700

    add pretty json

---
+++ Pretty JSON/PrettyJson.py
@@ -0,0 +1,23 @@
+import sublime
+import sublime_plugin
+import json
+
+s = sublime.load_settings("Pretty JSON.sublime-settings")
+
+
+class PrettyjsonCommand(sublime_plugin.TextCommand):
+    """ Pretty Print JSON
+    """
+    def run(self, edit):
+        for region in self.view.sel():
+            # If no selection, use the entire file as the selection
+            if region.empty() and s.get("use_entire_file_if_no_selection"):
+                selection = sublime.Region(0, self.view.size())
+            else:
+                selection = region
+
+            try:
+                obj = json.loads(self.view.substr(selection))
+                self.view.replace(edit, selection, json.dumps(obj, indent=s.get("indent_size", 4), ensure_ascii=False, sort_keys=s.get("sort_keys", True), separators=(',', ': ')))
+            except Exception, e:
+                sublime.status_message(str(e))
