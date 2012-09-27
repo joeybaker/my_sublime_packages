f23f4b5397ec185a2844198c9a0a05f0906cd492
Joey Baker <joey@byjoeybaker.com>
2012-09-26 17:00:44 -0700

    auto-updates

--- Markdown Preview/MarkdownPreview.py
+++ Markdown Preview/MarkdownPreview.py
@@ -3,7 +3,12 @@ import desktop
 import tempfile
 import markdown2
 import os
+import sys
 import re
+import json
+import urllib2
+
+settings = sublime.load_settings('MarkdownPreview.sublime-settings')
 
 
 def getTempMarkdownPreviewPath(view):
@@ -22,22 +27,40 @@ class MarkdownPreviewListener(sublime_plugin.EventListener):
             if os.path.isfile(temp_file):
                 # reexec markdown conversion
                 view.run_command('markdown_preview', {'target': 'disk'})
+                sublime.status_message('Markdown preview file updated')
+
+
+class MarkdownCheatsheetCommand(sublime_plugin.TextCommand):
+    """ open our markdown cheat sheet in ST2"""
+    def run(self, edit):
+        cheatsheet = os.path.join(sublime.packages_path(), 'Markdown Preview', 'sample.md')
+        self.view.window().open_file(cheatsheet)
+        sublime.status_message('Markdown cheat sheet opened')
 
 
 class MarkdownPreviewCommand(sublime_plugin.TextCommand):
     """ preview file contents with python-markdown and your web browser"""
 
     def getCSS(self):
-        css_filename = 'markdown.css'
-        # path via package manager
-        css_path = os.path.join(sublime.packages_path(), 'Markdown Preview', css_filename)
-        if not os.path.isfile(css_path):
-            # path via git repo
-            css_path = os.path.join(sublime.packages_path(), 'sublimetext-markdown-preview', css_filename)
-            if not os.path.isfile(css_path):
-                raise Exception("markdown.css file not found!")
+        config_parser = settings.get('parser')
+        config_css = settings.get('css')
 
-        return open(css_path, 'r').read().decode('utf-8')
+        if config_css and config_css != 'default':
+            return "<link href='%s' rel='stylesheet' type='text/css'>" % config_css
+        else:
+            css_filename = 'markdown.css'
+            if config_parser and config_parser == 'github':
+                css_filename = 'github.css'
+            # path via package manager
+            css_path = os.path.join(sublime.packages_path(), 'Markdown Preview', css_filename)
+            if not os.path.isfile(css_path):
+                # path via git repo
+                css_path = os.path.join(sublime.packages_path(), 'sublimetext-markdown-preview', css_filename)
+                if not os.path.isfile(css_path):
+                    sublime.error_message('markdown.css file not found!')
+                    raise Exception("markdown.css file not found!")
+            styles = u"<style>%s</style>" % open(css_path, 'r').read().decode('utf-8')
+        return styles
 
     def postprocessor(self, html):
         # fix relative paths in images/scripts
@@ -62,11 +85,29 @@ class MarkdownPreviewCommand(sublime_plugin.TextCommand):
             encoding = 'windows-1252'
         contents = self.view.substr(region)
 
-        # convert the markdown
-        markdown_html = markdown2.markdown(contents, extras=['footnotes', 'toc'])
+        config_parser = settings.get('parser')
 
-        # postprocess the html
-        markdown_html = self.postprocessor(markdown_html)
+        markdown_html = u'cannot convert markdown'
+        if config_parser and config_parser == 'github':
+            sublime.status_message('converting markdown with github API...')
+            try:
+                contents = contents.replace('%', '')    # see https://gist.github.com/3742011
+                data = json.dumps({"text": contents, "mode": "gfm"})
+                url = "https://api.github.com/markdown"
+                markdown_html = urllib2.urlopen(url, data).read().decode('utf-8')
+            except urllib2.HTTPError:
+                sublime.error_message('github API responded in an unfashion way :/')
+            except urllib2.URLError:
+                sublime.error_message('cannot use github API to convert markdown. SSL is not included in your Python installation')
+            except:
+                sublime.error_message('cannot use github API to convert markdown. Please check your settings.')
+            else:
+                sublime.status_message('converted markdown with github API successfully')
+        else:
+            # convert the markdown
+            markdown_html = markdown2.markdown(contents, extras=['footnotes', 'toc', 'fenced-code-blocks', 'cuddled-lists', 'code-friendly'])
+            # postprocess the html
+            markdown_html = self.postprocessor(markdown_html)
 
         # check if LiveReload ST2 extension installed
         livereload_installed = ('LiveReload' in os.listdir(sublime.packages_path()))
@@ -74,8 +115,7 @@ class MarkdownPreviewCommand(sublime_plugin.TextCommand):
         # build the html
         html_contents = u'<!DOCTYPE html>'
         html_contents += '<html><head><meta charset="%s">' % encoding
-        styles = self.getCSS()
-        html_contents += '<style>%s</style>' % styles
+        html_contents += self.getCSS()
         if livereload_installed:
             html_contents += '<script>document.write(\'<script src="http://\' + (location.host || \'localhost\').split(\':\')[0] + \':35729/livereload.js?snipver=1"></\' + \'script>\')</script>'
         html_contents += '</head><body>'
@@ -90,10 +130,23 @@ class MarkdownPreviewCommand(sublime_plugin.TextCommand):
             tmp_html.close()
             # todo : livereload ?
             if target == 'browser':
-                desktop.open(tmp_fullpath)
+                config_browser = settings.get('browser')
+                if config_browser and config_browser != 'default':
+                    cmd = '"%s" %s' % (config_browser, tmp_fullpath)
+                    if sys.platform == 'darwin':
+                        cmd = "open -a %s" % cmd
+                    print "Markdown Preview: executing", cmd
+                    result = os.system(cmd)
+                    if result != 0:
+                        sublime.error_message('cannot execute "%s" Please check your Markdown Preview settings' % config_browser)
+                    else:
+                        sublime.status_message('Markdown preview launched in %s' % config_browser)
+                else:
+                    desktop.open(tmp_fullpath)
+                    sublime.status_message('Markdown preview launched in default html viewer')
         elif target == 'sublime':
             new_view = self.view.window().new_file()
             new_edit = new_view.begin_edit()
             new_view.insert(new_edit, 0, html_contents)
             new_view.end_edit(new_edit)
-        print 'markdown converted'
+            sublime.status_message('Markdown preview launched in sublime')
