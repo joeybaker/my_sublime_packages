85fbf00bd0a3dda118242d3cc33ee480febe965b
Joey Baker <joey@meraki.com>
2013-01-25 14:06:16 -0800

    Add CaseConversion

---
+++ Case Conversion/case_conversion.py
@@ -0,0 +1,77 @@
+import sublime_plugin
+import re
+
+
+def to_snake_case(text):
+    text = re.sub('[-. _]+', '_', text)
+    if text.isupper():
+        # Entirely uppercase; assume case is insignificant.
+        return text.lower()
+    return re.sub('(?<=[^_])([A-Z])', r'_\1', text).lower()
+
+
+def strip_wrapping_underscores(text):
+    return re.sub("^(_*)(.*?)(_*)$", r'\2', text)
+
+
+def to_pascal_case(text):
+    callback = lambda pat: pat.group(1).upper()
+    text = re.sub("_(\w)", callback, text)
+    if text[0].islower():
+        text = text[0].upper() + text[1:]
+    return text
+
+
+def to_camel_case(text):
+    text = to_pascal_case(text)
+    return text[0].lower() + text[1:]
+
+
+def to_dot_case(text):
+    return text.replace("_", ".")
+
+
+def to_dash_case(text):
+    return text.replace("_", "-")
+
+
+def to_separate_words(text):
+    return text.replace("_", " ")
+
+
+def run_on_selections(view, edit, func):
+    for s in view.sel():
+        region = s if s else view.word(s)
+        text = to_snake_case(view.substr(region))
+        text = strip_wrapping_underscores(text)
+        view.replace(edit, region, func(text))
+
+
+class ConvertToSnakeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        run_on_selections(self.view, edit, lambda text: text)
+
+
+class ConvertToCamel(sublime_plugin.TextCommand):
+    def run(self, edit):
+        run_on_selections(self.view, edit, to_camel_case)
+
+
+class ConvertToPascal(sublime_plugin.TextCommand):
+    def run(self, edit):
+        run_on_selections(self.view, edit, to_pascal_case)
+
+
+class ConvertToDot(sublime_plugin.TextCommand):
+    def run(self, edit):
+        run_on_selections(self.view, edit, to_dot_case)
+
+
+class ConvertToDash(sublime_plugin.TextCommand):
+    def run(self, edit):
+        run_on_selections(self.view, edit, to_dash_case)
+
+
+class ConvertToSeparateWords(sublime_plugin.TextCommand):
+    def run(self, edit):
+        run_on_selections(self.view, edit, to_separate_words)
---
+++ Case Conversion/readme.md
@@ -0,0 +1,46 @@
+# Case Conversion
+Case conversion is a plugin for Sublime Text 2. It converts the current word/token between pascal, 
+camel, snake, dot, dash (hyphen) cases, and separated words.
+
+## Keybindings
+- To snake_case:  "ctrl+alt+c", "ctrl+alt+s"  
+- to camelCase: "ctrl+alt+c", "ctrl+alt+c"
+- to PascalCase: "ctrl+alt+c", "ctrl+alt+p"
+- to dot.case: "ctrl+alt+c", "ctrl+alt+d"
+- to dash-case: "ctrl+alt+c", "ctrl+alt+h"
+- to separate words: "ctrl+alt+c", "ctrl+alt+w"
+
+## Install
+#### Git Clone
+Clone this repository in to the Sublime Text 2 "Packages" directory, which is located where ever the 
+"Preferences" -> "Browse Packages" option in sublime takes you.
+
+#### Package Control
+Add https://github.com/jdc0589/CaseConversion as a Package Control repository. CaseConversion will show up in the
+package install list.
+
+## Contributors
+- Davis Clark
+- Scott Bessler
+- Curtis Gibby
+
+## License
+Copyright (C) 2012 Davis Clark
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
