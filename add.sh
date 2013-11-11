7ee31f8328c38c4c7aa71056c77f5d1a33d06e32
Joey Baker <joey.baker@ubnt.com>
2013-11-11 13:40:57 -0800

    Update script needs to know the branch

--- add.sh
+++ add.sh
@@ -4,3 +4,4 @@ rm -rf Packages/$1
 git remote add $1 $2;
 git fetch $1
 git subtree add --prefix Packages/$1 $1/$3 --squash
+echo "$1=$3" >> .subtrees
--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+source ./.subtrees
+
 for remote in `git remote | grep -v origin | tr "\\n" " "`; do
-  git subtree pull --prefix Packages/$remote $remote --squash
+  git subtree pull --prefix Packages/$remote $remote ${!remote} --squash
 done;
 
